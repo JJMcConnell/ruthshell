daf36df6b6c97886aa87b976d384ae2cdf9bf804
Aaron Abbott <aabmass@gmail.com>
2015-04-10 02:59:08 -0400

    More error handling
    Still need to add much more!

--- src/ruthshell.c
+++ src/ruthshell.c
@@ -55,14 +55,25 @@ int runCmdAndFreeStrings(void) {
     // first create fork for new process
     pid_t subProc = fork();
 
-    if (subProc == 0) { // if child process
+    if (subProc == -1) { // forking error!
+        /* TODO: implement some better error handling with return vals */
+        printf("Error: could not fork\n");
+        return -1;
+    }
+    else if (subProc == 0) { // if child process
         // execute the command with argv
-        execvpe(cmd, argv, environ);
+        // this will only truly exit if there is an error
+        _exit(execvpe(cmd, argv, environ));
     }
     else { // if shell process
         // wait for the forked process to finish
         int status;
         waitpid(subProc, &status, 0);
+        int retVal = WEXITSTATUS(status);
+        if (retVal != 0)
+            printf("Error: command return %i\n", retVal);
+
+        // do some smart error handling with status
     }
 
     popArgsFreeStrings(); // free argv
